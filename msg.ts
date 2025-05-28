b1e36480180accce0fc22a97b7c9a3095afe50f9
Louie Torres <dev@lowkeytowers.com>
2025-05-28 15:56:08 +0800

    feat(msg): redefine Msg according to mvu/canvas

--- msg.ts
+++ msg.ts
@@ -1,15 +1,5 @@
+import { MsgKeyDown, MsgMouseDown, MsgTick } from "cs12242-mvu/src/canvas";
 import { Schema as S } from "effect";
-import { Egg, Model, initModel } from "./model";
 
+export const Msg = S.Union(MsgKeyDown, MsgMouseDown, MsgTick);
 export type Msg = typeof Msg.Type;
-export const Msg = S.Union(
-  //asynch events/interactions with the site
-  S.TaggedStruct("MsgKeyUp", { key: S.String }),
-  S.TaggedStruct("MsgKeyDown", { key: S.String }),
-  S.TaggedStruct("MsgKeyTick", { key: S.String }),
-  S.TaggedStruct("MsgError", { error: S.String }),
-);
-
-export const [MsgKeyUp, MsgKeyDown, MsgKeyTick, MsgError] = Msg.members;
-//   S.TaggedStruct("MsgKeyLeft", {key: S.String}),
-//   S.TaggedStruct("MsgKeyRight", {key: S.String}),
