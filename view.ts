700816a34382d8450d15f00faf7bf3c60127bde2
Louie Torres <dev@lowkeytowers.com>
2025-05-28 15:15:48 +0800

    feat(view): migrate to canvasView

--- view.ts
+++ view.ts
@@ -1,61 +1,39 @@
-import { h } from "cs12242-mvu/src";
+// Not sure if this is allowed as of now, but I hope it is.
+import {
+  canvasView,
+  Clear,
+  SolidRectangle,
+  Text,
+  type CanvasElement,
+} from "cs12242-mvu/src/canvas";
 import { Model } from "./model";
-import { Msg } from "./msg";
-import { Array } from "effect";
+import type { Msg } from "./msg";
 
-export const [MsgKeyUp, MsgKeyDown, MsgKeyTick, MsgError] = Msg.members;
-
-export const view = (model: Model, dispatch: (msg: Msg) => void) =>
-  h("div", [
-    h("canvas", {
-      props: {
-        width: 800,
-        height: 600,
-      },
-      style: {
-        background: "black",
-      },
-      hook: {
-        insert: (vnode) => {
-          const canvas = vnode.elm as HTMLCanvasElement;
-          const ctx = canvas.getContext("2d")!;
-
-          ctx.fillStyle = "black";
-          ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-          ctx.fillStyle = "white";
-          ctx.fillRect(
-            model.egg.x,
-            model.egg.y,
-            model.egg.width,
-            model.egg.height,
-          );
-
-          ctx.fillStyle = "red";
-          ctx.font = "16px sans-serif";
-          ctx.fillText(`HP: ${model.egg.hp}/${model.egg.maxHp}`, 10, 20);
-        },
-
-        update: (oldVNode, newVNode) => {
-          //how to draw the state on the screen
-          const canvas = newVNode.elm as HTMLCanvasElement;
-          const ctx = canvas.getContext("2d")!;
-
-          ctx.fillStyle = "black";
-          ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-          ctx.fillStyle = "white";
-          ctx.fillRect(
-            model.egg.x,
-            model.egg.y,
-            model.egg.width,
-            model.egg.height,
-          );
-
-          ctx.fillStyle = "red";
-          ctx.font = "16px sans-serif";
-          ctx.fillText(`HP: ${model.egg.hp}/${model.egg.maxHp}`, 10, 20);
-        },
-      },
-    }),
-  ]); // closing bracket for the whole div
+export const view = canvasView<Model, Msg>(
+  800,
+  600,
+  60,
+  "mpDisplay",
+  (model: Model): CanvasElement[] => {
+    return [
+      Clear.make({
+        color: "black",
+      }),
+      SolidRectangle.make({
+        color: "white",
+        x: model.egg.x,
+        y: model.egg.y,
+        width: model.egg.width,
+        height: model.egg.height,
+      }),
+      Text.make({
+        color: "red",
+        font: "sans-serif",
+        fontSize: 16,
+        x: 10,
+        y: 20,
+        text: `HP: ${model.egg.hp}/${model.egg.maxHp}`,
+      }),
+    ];
+  },
+);
